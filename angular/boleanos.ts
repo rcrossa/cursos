64961e09bf929f8f0d8ca7e6b3fa8a456d47e334
Roberto Rossa <rcrossa@hotmail.com>
2020-04-12 20:24:35 -0300

    update

---
+++ angular/boleanos.ts
@@ -0,0 +1,14 @@
+var esnuevo:boolean= false;
+var deseaImprimir:boolean = true;
+
+var juan = "juan";
+var juan2= "juan";
+var aceptaDescuento:boolean =(15+19==33);
+
+
+var sonNombresIguales:boolean=(juan==juan2)
+console.log("Este booleano es: ",aceptaDescuento)
+console.log("Este booleano es: " ,sonNombresIguales)
+
+var sonAmbosVerdaderos:boolean = sonNombresIguales && aceptaDescuento
+console.log("El resultado de dos booleanos: ",sonAmbosVerdaderos)
