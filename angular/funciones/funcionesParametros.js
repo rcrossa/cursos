639ea3b16fba6f922d6229d2f067d3554c36a0f9
Roberto Rossa <rcrossa@hotmail.com>
2020-04-14 16:13:04 -0300

    update

---
+++ angular/funciones/funcionesParametros.js
@@ -0,0 +1,9 @@
+function mostrarMensaje(mensaje) {
+    console.log(mensaje);
+}
+mostrarMensaje("Prueba de mensaje");
+function calcularSuma(numero1, numero2) {
+    var total = numero1 + numero2;
+    console.log(total);
+}
+calcularSuma(15, 95);
