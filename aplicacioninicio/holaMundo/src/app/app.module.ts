ed41dcbd96e218b66bf67f230a02f97fc4c1286b
Roberto Rossa <rcrossa@hotmail.com>
2020-04-15 18:35:09 -0300

    creando app angular

--- aplicacioninicio/holaMundo/src/app/app.module.ts
+++ aplicacioninicio/holaMundo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitulosComponent } from './titulos/titulos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitulosComponent
   ],
   imports: [
     BrowserModule,
