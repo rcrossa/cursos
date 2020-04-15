4f2cc0a6cf9c9b601a7985218efb96cb2472c57c
Roberto Rossa <rcrossa@hotmail.com>
2020-04-15 15:03:12 -0300

    update

---
+++ typeScritp/funciones/funcionesDatos.ts
@@ -0,0 +1,18 @@
+function multiplicar(numero1:number,numero2:number): number {
+    var total = numero1 * numero2;
+    return total;
+
+}
+function calcularPromedio(nota1:number, nota2:number,nota3:number) : number{
+    var total: number =(nota1+nota2+nota3);
+}
+
+var numeroResultado: number =0;
+numeroResultado = multiplicar(5,10);
+
+var promedio:number =0;
+
+promedio = calcularPromedio(60,80,90);
+
+
+console.log(promedio);
