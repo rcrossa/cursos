9b10f32bc11ac65012fa4ba2ab927f5c48746cc6
Roberto Rossa <rcrossa@hotmail.com>
2020-05-30 10:12:39 -0300

    nuevos ejercicios

---
+++ poo_davinci/sobrecargademetodos/src/sobrecargademetodos/Metodotest.java
@@ -0,0 +1,23 @@
+package sobrecargademetodos;
+
+public class Metodotest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sumar a = new Sumar();
+		Sumar b = new Sumar();
+		Sumar c = new Sumar();
+		Sumar d = new Sumar();
+		Sumar e = new Sumar();
+		
+		
+		System.out.println(a.sumar(2, 3));
+		System.out.println(b.sumar(40, 30));
+		System.out.println(c.sumar(50, 15));
+		System.out.println(d.sumar(20, 20));
+		System.out.println(e.sumar(10, 20));
+		
+		
+	}
+	
+}
---
+++ poo_davinci/sobrecargademetodos/src/sobrecargademetodos/Sumar.java
@@ -0,0 +1,22 @@
+package sobrecargademetodos;
+
+public class Sumar {
+
+	 public int sumar(int sumUno, int sumDos)   {
+		 return sumUno + sumDos;
+	 }
+	 public double sumar(int sumUno,double sumDos) {
+		 return sumUno + sumDos;
+	 }
+	 public float sumar(float sumUno,float sumDos){
+		 return sumUno + sumDos;
+	 }
+	 public float sumar(float sumUno,int sumDos)  {
+		 return sumUno + sumDos;
+	 }
+	 public double sumar(double sumUno, int sumDos){
+		 return sumUno + sumDos;
+ 	 }
+	 
+}
+
