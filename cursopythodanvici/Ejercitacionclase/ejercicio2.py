a1d2fd6d3132851007e9bae4f8d2c78208e61018
Roberto Rossa <rcrossa@hotmail.com>
2020-06-06 16:37:19 -0300

    actualizacion de ejercicio

--- cursopythodanvici/Ejercitacionclase/ejercicio2.py
+++ cursopythodanvici/Ejercitacionclase/ejercicio2.py
@@ -1,81 +1,77 @@
 import funcionesejercicios
+from random import randint
 
 
-
-# n=numerorandom()
-a=[1,2,5]
+# n=int(randint(0,100))
+a=['1','2','5']
+n=0
 resul=0
+resul2=0
+resul3=0
 monedas5=0
 monedas2=0
 monedas1=0
 resul=0
 a=0
-n=0
+# n=0
 n=funcionesejercicios.numerorandom(n)
-valores=0
+# valores=0
 
 
 
 print("Numero random: ",n)
 
-resul=funcionesejercicios.numero5(resul)
-# if resul < n:
-#     if 5 in a:
-#         while resul < n:
-#             resul+=5
-#             monedas5+=1
 
 
-    
-# print("primer cantidad:",resul)
-# resul=5*monedas5
-# print("Primer result:",resul)
-
-if(resul > n):
-    monedas5=monedas5-1
-    print("monedas:",monedas5)
-    resul=resul-5
-    resul=5*monedas5
-    print("Segundo result:",resul)
-
-
-    if resul<n:
-        if 2 in a:
-            while resul < n:
-                resul+=2
-                monedas2=monedas2+1
-
-    resul2=2*monedas2
-        #  resul=resul+resul2
-
-# print("Segunda cantidad:",resul)
-
-
-if(resul>n):
-   while resul>n:
-        monedas2=monedas2-1
-        resul=resul-2
-
-    # resul2=2*monedas2
-    # resul=resul+resul2
-    # print("monedas if:",monedas5)
-# print("Segundo result:",resul)
-
-for e in a:
-    if e == 1:
-        break
-    print(e)
-
-if resul < n :
-    if 1 in a:
-        while resul < n:
-            resul+=1
-            monedas1+=1    
-
-# resul3=1*monedas1
-# resul=resul+resul3
-
-# print("resultado final:\n", resul)
-
+if resul < n:
+            for a in range(3):
+                    if a%5==0:
+                        while resul < n:
+                                resul+=5
+                                monedas5+=1
+                    if(resul > n):
+                        monedas5=monedas5-1
+                        # print("monedas 5:",monedas5)
+                        resul=resul-5
+                        resul=5*monedas5
+                        # print("Segundo result:",resul)
+            for a in range(3):      
+                    if a%2==a:
+                        while resul < n:
+                            resul+=2
+                            monedas2=monedas2+1
+                    if(resul>n):
+                             while resul>n:
+                                    monedas2=monedas2-1
+                                    resul=resul-2
+            for a in range(3):
+                    if a%2==a:
+                        while resul < n:
+                            resul+=1
+                            monedas1+=1  
+                    resul3=1*monedas1
+                    resul=resul+resul3    
 
 print("Monedas de 5: ",monedas5,"\nMonedas de 2: ", monedas2,"\nMonedas de 1:", monedas1)
+
+
+
+def resta(monedas5):
+        monedas5=monedas5-1
+        print("monedas 5:",monedas5)
+        resul=resul-5
+        resul=5*monedas5
+        print("Segundo result:",resul)
+
+
+def suma (monedas5):
+    resul=5*monedas5
+    return resul
+
+def suma (monedas2):
+     resul=5*monedas5
+     return resul
+
+def suma (monedas1):
+    resul=5*monedas5
+    return resul
--- cursopythodanvici/Ejercitacionclase/funcionesejercicios.py
+++ cursopythodanvici/Ejercitacionclase/funcionesejercicios.py
@@ -2,11 +2,12 @@ from collections import Counter
 from random import randint
 
 a=[1,2,5]
-
+resul=0
 n=0
 monedas5=0
 monedas2=0
 monedas1=0
+
 
 # Ejercicio 1
 def separacaracteres(b):
@@ -19,18 +20,46 @@ def contador(b):
 # Ejercicio 2
 
 def numerorandom(n):
-            numero=int(randint(0,100))
-            return numero
+            n=int(randint(0,100))
+            return n
 
 
 
 def numero5(resul):
-        if resul < n:
-            if 5 in a:
+         if resul < n:
+            for a in range(3):
+                    if a%5==0:
+                        while resul < n:
+                                resul+=5
+                                monedas5+=1
+                                
+                                return resul
+
+
+def numero2(resul):
+        if resul<numerorandom:
+            if resul%2 in a:
                 while resul < n:
-                        resul+=5
-                        monedas5+=1
-                        return resul
+                    resul+=2
+                    monedas2=monedas2+1
+                    return resul
 
+def numero1(resul):
+        if resul<n:
+            if 2 in a:
+                while resul < n:
+                    resul+=2
+                    monedas2=monedas2+1
+                    
 
+def suma (monedas5):
+    resul=5*monedas5
+    return resul
 
+def suma (monedas2):
+     resul=5*monedas5
+     return resul
+
+def suma (monedas1):
+    resul=5*monedas5
+    return resul
